b97971f0da797bf0563ada51d56d027cf50716c4
softpo <455098435@qq.com>
2016-11-16 15:23:41 +0800

    声明了实体类Softpo

--- app/src/main/java/com/softpo/teaencyclopedia/Softpo.java
+++ app/src/main/java/com/softpo/teaencyclopedia/Softpo.java
@@ -5,4 +5,22 @@ package com.softpo.teaencyclopedia;
  */
 
 public class Softpo {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
